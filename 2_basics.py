31dc532e938c44ef9dd8c81969d1163533563e60
jahangirinegin94 <jahangiri.negin94@gmail.com>
2025-12-10 10:27:46 +0330

    update 2_basics.py

--- 2_basics.py
+++ 2_basics.py
@@ -6,3 +6,29 @@ ctrl + /
 
 # شکستن خط
 alt + z
+
+#Name
+print(name.title())
+
+#NAME
+print(name.upper())
+
+#name
+print(NAME.lower())
+
+#replace
+text1=...
+text2=text1.replace(...,___)
+
+#count
+C=DNA_Seq.count('C')
+
+#Division
+GC_count=(G+C)/len(DNA_Seq)*100
+print(GC_count)
+
+#گرد کردن اعداد
+#3 -> تعداد رقم اعشار
+print(round(variable,3))
+
+
